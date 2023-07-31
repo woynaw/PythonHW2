9ddd76cf0dd200f3b4ef10b7c05067129d772149
Войналович Владислав <woynaw@hotmail.com>
2023-07-31 09:57:43 +0300

    Initial commit

--- zadacha24.py
+++ zadacha24.py
@@ -5,6 +5,6 @@ kust = [int(input()) for item in range(n)]
 max = 0
 kust = kust + kust[:1]
 for i in range(n):
-    if (kust[i] + kust[i + 1] + kust[i - 1]) > max:
-        max = kust[i] + kust[i + 1] + kust[i - 1]
+    if (kust[i] + kust[i - 1] + kust[i + 1]) > max:
+        max = kust[i] + kust[i - 1] + kust[i + 1]
 print(max)
